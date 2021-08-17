435a8bc28569fe4d9634a13b5c32935dc6bcd0fc
Clifford Morin <cemorin21@gmail.com>
2021-08-17 05:48:20 -0400

    added music player app

---
+++ src/views/MusicPlayPage/MusicPlayPage.js
@@ -0,0 +1,74 @@
+import React, { useState, useRef } from 'react';
+//Adding Components
+import Player from './components/Player';
+import Song from './components/Song';
+import Library from './components/Library';
+import Nav from './components/Nav';
+//Import Styles
+import './styles/app.scss';
+//Import Data
+import data from './data';
+
+function MusicPlayPage() {
+	//Ref
+	const audioRef = useRef(null);
+	//State
+	const [ songs, setSongs ] = useState(data());
+	const [ currentSong, setCurrentSong ] = useState(songs[0]);
+	const [ isPlaying, setIsPlaying ] = useState(false);
+	const [ songInfo, setSongInfo ] = useState({
+		currentTime: 0,
+		duration: 0,
+		animationPercentage: 0
+	});
+	const [ libraryStatus, setLibraryStatus ] = useState(false);
+	//handler
+	const timeUpdateHandler = (e) => {
+		const current = e.target.currentTime;
+		const duration = e.target.duration;
+		//Calculate percentage for animation
+		const roundedCurrent = Math.round(current);
+		const roundedDuration = Math.round(duration);
+		const animation = Math.round(roundedCurrent / roundedDuration * 100);
+		setSongInfo({ ...songInfo, currentTime: current, duration: duration, animationPercentage: animation });
+	};
+	const songEndHandler = async () => {
+		let currentIndex = songs.findIndex((song) => song.active);
+		await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+		if (isPlaying) audioRef.current.play();
+	};
+	return (
+		<div className={`App ${libraryStatus ? 'library-active' : ''}`}>
+			<Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+			<Song currentSong={currentSong} />
+			<Player
+				setSongInfo={setSongInfo}
+				songInfo={songInfo}
+				audioRef={audioRef}
+				setIsPlaying={setIsPlaying}
+				isPlaying={isPlaying}
+				currentSong={currentSong}
+				songs={songs}
+				setCurrentSong={setCurrentSong}
+				setSongs={setSongs}
+			/>
+			<Library
+				setSongs={setSongs}
+				isPlaying={isPlaying}
+				audioRef={audioRef}
+				songs={songs}
+				setCurrentSong={setCurrentSong}
+				libraryStatus={libraryStatus}
+			/>
+			<audio
+				onTimeUpdate={timeUpdateHandler}
+				onLoadedMetadata={timeUpdateHandler}
+				ref={audioRef}
+				src={currentSong.audio}
+				onEnded={songEndHandler}
+			/>
+		</div>
+	);
+}
+
+export default MusicPlayPage;
---
+++ src/views/MusicPlayPage/data.js
@@ -0,0 +1,78 @@
+import { v4 as uuidv4 } from 'uuid';
+function chillHop() {
+	return [
+		{
+			name: 'Beaver Creek',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg',
+			artist: 'Aso, Middle School, Aviino',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=10075',
+			color: [ '#205950', '#2ab3bf' ],
+			id: uuidv4(),
+			active: true
+		},
+		{
+			name: 'Daylight',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg',
+			artist: 'Aiguille',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=9272',
+			color: [ '#EF8EA9', '#ab417f' ],
+			id: uuidv4(),
+			active: false
+		},
+		{
+			name: 'Keep Going',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg',
+			artist: 'Swørn',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=9222',
+			color: [ '#CD607D', '#c94043' ],
+			id: uuidv4(),
+			active: false
+		},
+		{
+			name: 'Nightfall',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg',
+			artist: 'Aiguille',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=9148',
+			color: [ '#EF8EA9', '#ab417f' ],
+			id: uuidv4(),
+			active: false
+		},
+		{
+			name: 'Reflection',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg',
+			artist: 'Swørn',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=9228',
+			color: [ '#CD607D', '#c94043' ],
+			id: uuidv4(),
+			active: false
+		},
+		{
+			name: 'Under the City Stars',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg',
+			artist: 'Aso, Middle School, Aviino',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=10074',
+			color: [ '#205950', '#2ab3bf' ],
+			id: uuidv4(),
+			active: false
+		},
+		//ADD MORE HERE
+		{
+			name: 'sozu',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2021/05/1245c0271290a3196328c0cf4aaa910cd873dfe7-1024x1024.jpg',
+			artist: 'mommy, Sleepy Fish',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=19062',
+			color: [ '#598CCE', '#EDD6D2' ],
+			id: uuidv4(),
+			active: false
+		}
+	];
+}
+
+export default chillHop;
