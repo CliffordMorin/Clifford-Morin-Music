ad350ca951515cb894a560668d42c0878a6b5011
Clifford Morin <cemorin21@gmail.com>
2021-08-21 14:14:02 -0400

    added snackbar to confirm email was sent

--- src/views/ContactPage/Sections/EmailSection.js
+++ src/views/ContactPage/Sections/EmailSection.js
@@ -3,12 +3,16 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 // @material-ui/icons
-
+import Check from '@material-ui/icons/Check';
 // core components
 import GridContainer from 'components/Grid/GridContainer.js';
 import GridItem from 'components/Grid/GridItem.js';
 import CustomInput from 'components/CustomInput/CustomInput.js';
 import Button from 'components/CustomButtons/Button.js';
+import SnackbarContent from 'components/Snackbar/SnackbarContent.js';
+import Clearfix from 'components/Clearfix/Clearfix.js';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 //emailJS
 import emailjs from 'emailjs-com';
@@ -20,15 +24,32 @@ const SERVICE_ID = process.env.REACT_APP_YOUR_SERVICE_ID;
 const TEMPLATE_ID = process.env.REACT_APP_YOUR_TEMPLATE_ID;
 const USER_ID = process.env.REACT_APP_YOUR_USER_ID;
 
+function Alert(props) {
+	return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
 export default function EmailSection() {
 	const classes = useStyles();
+	const [ open, setOpen ] = React.useState(false);
+
+	const handleClick = () => {
+		setOpen(true);
+	};
+
+	const handleClose = (event, reason) => {
+		if (reason === 'clickaway') {
+			return;
+		}
+
+		setOpen(false);
+	};
 	const sendEmail = (e) => {
 		e.preventDefault();
 
 		emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID).then(
 			(result) => {
 				console.log(result.text);
-				console.log(e.target);
+				handleClick();
 			},
 			(error) => {
 				console.log(error.text);
@@ -42,8 +63,8 @@ export default function EmailSection() {
 				<GridItem cs={12} sm={12} md={8}>
 					<h2 className={classes.title}>Send Me An Email</h2>
 					<h4 className={classes.description}>
-						If you would like to contact me regarding a private lesson or performing. Please send an email
-						below or at my email address cemorin21@gmail.com. Thank You!
+						For any inquiries regarding a private lesson or performing. Please send an email below or at my
+						email address cliffordmorinmusic@gmail.com. Thank You!
 					</h4>
 					<form onSubmit={sendEmail} className={classes.form} id="emailForm">
 						<GridContainer>
@@ -89,6 +110,11 @@ export default function EmailSection() {
 					</form>
 				</GridItem>
 			</GridContainer>
+			<Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+				<Alert onClose={handleClose} severity="success">
+					Email Sent!
+				</Alert>
+			</Snackbar>
 		</div>
 	);
 }
