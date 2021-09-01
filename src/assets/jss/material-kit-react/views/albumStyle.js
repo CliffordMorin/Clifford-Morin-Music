3311704a9029354c7aac5f0bb70e042da125cda9
Clifford Morin <cemorin21@gmail.com>
2021-09-01 17:59:53 -0400

    added dicography and video page, fixed video responsiveness

--- src/assets/jss/material-kit-react/views/albumStyle.js
+++ src/assets/jss/material-kit-react/views/albumStyle.js
@@ -17,6 +17,21 @@ const albumStyle = {
 		maxWidth: '100%',
 		height: 'auto',
 		paddingBottom: '20px'
+	},
+	video: {
+		top: '0',
+		left: '0',
+		width: '100%',
+		height: '100%',
+		paddingBottom: '20px',
+		position: 'absolute'
+	},
+	videoContainer: {
+		...container,
+		position: 'relative',
+		paddingBottom: '56.25%',
+		paddingTop: '25',
+		height: '0'
 	}
 };
 
