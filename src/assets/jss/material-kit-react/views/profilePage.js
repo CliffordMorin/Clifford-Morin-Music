a2df1bd3d128f2f6c8424681fc091e9521007705
Clifford Morin <cemorin21@gmail.com>
2021-09-02 20:06:17 -0400

    added something to profile

--- src/assets/jss/material-kit-react/views/profilePage.js
+++ src/assets/jss/material-kit-react/views/profilePage.js
@@ -16,7 +16,7 @@ const profilePageStyle = {
 	description: {
 		margin: '1.071rem auto 0',
 		maxWidth: '600px',
-		color: '#999',
+		color: '#000000',
 		textAlign: 'center !important'
 	},
 	name: {
