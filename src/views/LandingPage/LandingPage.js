5c390f2a03c6ed944b18659caed5f5c3bd371a77
Clifford Morin <cemorin21@gmail.com>
2023-01-14 01:26:42 -0500

    Revert "added more videos and changed home video"
    This reverts commit 3c7111b7c5a3e510b9905d5baa92f89b9214a313.

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -1,28 +1,28 @@
-import React from "react";
+import React from 'react';
 // nodejs library that concatenates classes
-import classNames from "classnames";
+import classNames from 'classnames';
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 // @material-ui/icons
 
 // core components
-import Header from "components/Header/Header.js";
-import Footer from "components/Footer/Footer.js";
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
-import Button from "components/CustomButtons/Button.js";
-import HeaderLinks from "components/Header/HeaderLinks.js";
-import Parallax from "components/Parallax/Parallax.js";
+import Header from 'components/Header/Header.js';
+import Footer from 'components/Footer/Footer.js';
+import GridContainer from 'components/Grid/GridContainer.js';
+import GridItem from 'components/Grid/GridItem.js';
+import Button from 'components/CustomButtons/Button.js';
+import HeaderLinks from 'components/Header/HeaderLinks.js';
+import Parallax from 'components/Parallax/Parallax.js';
 
 //custom components
-import TypedHook from "../Components/TypedHook";
+import TypedHook from '../Components/TypedHook';
 
-import styles from "assets/jss/material-kit-react/views/landingPage.js";
+import styles from 'assets/jss/material-kit-react/views/landingPage.js';
 
 // Sections for this page
-import ProductSection from "./Sections/ProductSection.js";
-import TeamSection from "./Sections/TeamSection.js";
+import ProductSection from './Sections/ProductSection.js';
+import TeamSection from './Sections/TeamSection.js';
 // import WorkSection from './Sections/WorkSection.js';
 
 const dashboardRoutes = [];
@@ -30,56 +30,52 @@ const dashboardRoutes = [];
 const useStyles = makeStyles(styles);
 
 export default function LandingPage(props) {
-  const classes = useStyles();
-  const { ...rest } = props;
-  return (
-    <div>
-      <Header
-        color="transparent"
-        routes={dashboardRoutes}
-        brand="Clifford Morin Music"
-        rightLinks={<HeaderLinks />}
-        fixed
-        changeColorOnScroll={{
-          height: 400,
-          color: "info",
-        }}
-        {...rest}
-      />
-      <Parallax
-        filter
-        className={classes.mainImg}
-        image={require("assets/img/cliffCloseUp/img3.jpeg").default}
-      >
-        <div className={classes.container}>
-          <GridContainer>
-            <GridItem xs={12} sm={12} md={6}>
-              <h1 className={classes.title}>Clifford Morin</h1>
-              <h3>
-                <TypedHook />
-              </h3>
-              <br />
-              <Button
-                color="info"
-                size="lg"
-                href="https://www.youtube.com/embed/kt4dzQMyJPE"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fas fa-play" />
-                Watch video
-              </Button>
-            </GridItem>
-          </GridContainer>
-        </div>
-      </Parallax>
-      <div className={classNames(classes.main, classes.mainRaised)}>
-        <div className={classes.container}>
-          <ProductSection />
-          <TeamSection />
-        </div>
-      </div>
-      <Footer />
-    </div>
-  );
+	const classes = useStyles();
+	const { ...rest } = props;
+	return (
+		<div>
+			<Header
+				color="transparent"
+				routes={dashboardRoutes}
+				brand="Clifford Morin Music"
+				rightLinks={<HeaderLinks />}
+				fixed
+				changeColorOnScroll={{
+					height: 400,
+					color: 'info'
+				}}
+				{...rest}
+			/>
+			<Parallax filter className={classes.mainImg} image={require('assets/img/cliffCloseUp/img3.jpeg').default}>
+				<div className={classes.container}>
+					<GridContainer>
+						<GridItem xs={12} sm={12} md={6}>
+							<h1 className={classes.title}>Clifford Morin</h1>
+							<h3>
+								<TypedHook />
+							</h3>
+							<br />
+							<Button
+								color="info"
+								size="lg"
+								href="https://www.youtube.com/watch?v=12gFQ2v3t18"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<i className="fas fa-play" />
+								Watch video
+							</Button>
+						</GridItem>
+					</GridContainer>
+				</div>
+			</Parallax>
+			<div className={classNames(classes.main, classes.mainRaised)}>
+				<div className={classes.container}>
+					<ProductSection />
+					<TeamSection />
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
 }
