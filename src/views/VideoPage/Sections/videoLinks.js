02d5e00e5d9f9e3d3499e5f19e6943a868631a60
Clifford Morin <cemorin21@gmail.com>
2023-01-14 02:04:26 -0500

    fixing deleted videoLinks.js

---
+++ src/views/VideoPage/Sections/videoLinks.js
@@ -0,0 +1,58 @@
+import react from "react";
+
+const videoLinks = [
+  {
+    link: "https://www.youtube.com/embed/xWrWFw0FzC8",
+    title: "Doux Amer Amor",
+  },
+  {
+    link: "https://www.youtube.com/embed/0_FfIa0wWOQ",
+    title: "Closed Doors",
+  },
+  {
+    link: "https://www.youtube.com/embed/DllgtjKxdE4",
+    title: "For those who are still here",
+  },
+  {
+    link: "https://www.youtube.com/embed/m0iGSkd-nUs",
+    title: "Slow",
+  },
+  {
+    link: "https://www.youtube.com/embed/7pU5ACmEip8",
+    title: "Sion",
+  },
+  {
+    link: "https://www.youtube.com/embed/kt4dzQMyJPE",
+    title: "Bound To Change",
+  },
+  {
+    link: "https://www.youtube.com/embed/Xrjm-_xskvY",
+    title: "Broadway Blues",
+  },
+  {
+    link: "https://www.youtube.com/embed/gnb_voZhySU",
+    title: "Three Flowers",
+  },
+  {
+    link: "https://www.youtube.com/embed/E9Qsb0Zfk9E",
+    title: "West of the west",
+  },
+  {
+    link: "https://www.youtube.com/embed/VfIwODN_Vfc",
+    title: "Soulful",
+  },
+  {
+    link: "https://www.youtube.com/embed/12gFQ2v3t18",
+    title: "Walk to the Guillotine",
+  },
+  {
+    link: "https://www.youtube.com/embed/Hcjy20E1Shg",
+    title: "Joey in the Pocket",
+  },
+  {
+    link: "https://www.youtube.com/embed/-TPttJ2l8iA",
+    title: "Barbara",
+  },
+];
+
+export default videoLinks;
