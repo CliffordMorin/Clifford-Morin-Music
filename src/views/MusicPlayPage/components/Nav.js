55fd3e6e455d3a2822bd016a80b2a947c346ee15
Clifford Morin <cemorin21@gmail.com>
2023-01-16 20:03:06 -0500

    fixed styling in music player

--- src/views/MusicPlayPage/components/Nav.js
+++ src/views/MusicPlayPage/components/Nav.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMusic } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMusic } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = ({ setLibraryStatus, libraryStatus }) => {
-	return (
-		<nav>
-			<h1>My Music</h1>
-			<button onClick={() => setLibraryStatus(!libraryStatus)}>
-				Library
-				<FontAwesomeIcon icon={faMusic} />
-			</button>
-		</nav>
-	);
+  return (
+    <nav>
+      <h1>My Music</h1>
+      <button onClick={() => setLibraryStatus(!libraryStatus)}>
+        <p>{libraryStatus ? "Close Library" : "Open Library"}</p>
+        <FontAwesomeIcon icon={faMusic} />
+      </button>
+    </nav>
+  );
 };
 
 export default Nav;
